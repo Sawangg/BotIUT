0d3b3c5dfd00b949d8ac971be09268804f280f3f
Sawangg <le1112osee@gmail.com>
2022-09-08 18:44:14 +0200

    v3.1.0 Fixed missing interface

--- src/interfaces/commands.ts
+++ src/interfaces/commands.ts
@@ -1,7 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type { ChatInputCommandInteraction } from "discord.js";
+import type { ApplicationCommandData, ChatInputCommandInteraction } from "discord.js";
 import type Bot from "../classes/client";
 
 export interface RunInterface {
     (client: Bot, interaction: ChatInputCommandInteraction): any;
+}
+
+export interface Command {
+    run: RunInterface;
+    interaction: ApplicationCommandData;
 }
